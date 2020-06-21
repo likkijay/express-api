fcd554ddb79974f3c9f5040b93350c401dffea2b
alex <likkijay@gmail.com>
2020-06-21 19:36:54 +0500

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import cookieParser from 'cookie-parser';
+import express from 'express';
+import logger from 'morgan';
+
+import indexRouter from './routes/index';
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use('/v1', indexRouter);
+
+export default app;
